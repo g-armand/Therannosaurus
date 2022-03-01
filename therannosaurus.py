f212c7c419a95a348ea033c7e7697b50920de31b
g-armand <78274164+g-armand@users.noreply.github.com>
2022-03-01 18:25:08 +0100

    Update therannosaurus.py
    ajout des fonctions: cos_similarity() et make_vectors()
    ajout d'un code à la fin qui affiche toutes les paires de vecteurs dont la similarité est comprise entre 0.85 et 1

--- therannosaurus.py
+++ therannosaurus.py
@@ -8,7 +8,7 @@ thesaurus = dict()
 
 def load_data():
         global thesaurus
-        with open("corpus.txt", "r", encoding='utf-8', errors='ignore') as corpus:   #Chargement du corpus (nom du fichier potentiellement à changer)
+        with open("corpus_10K.txt", "r", encoding='utf-8', errors='ignore') as corpus:   #Chargement du corpus (nom du fichier potentiellement à changer)
                 lines = corpus.readlines()
 
         for i in range(len(lines)):      #Pour chaque mot:
@@ -25,10 +25,62 @@ def load_data():
                                 continue                        #On ne s'intéresse au voisin que s'il ne s'agit pas d'une ligne vide (c'est mieux)
                         voisin = splitj[2]                                      #On stocke le lemme du mot voisin
                         thesaurus[(lemme, cat)][(voisin, j)] = thesaurus[(lemme, cat)].get((voisin, j), 0) + 1      #On stocke dans l'entrée lemme/cat le nombre d'apparition du voisin à la même position
-
-load_data()
-
+"""
 for key in thesaurus.keys():
         print("(" + key[0] + ", " + key[1] + ") : ")
         for keyInt in thesaurus[key].keys():
                 print("\t(" + keyInt[0] + ", " + str(keyInt[1]) + ") : " + str(thesaurus[key][keyInt]))
+"""
+
+def make_vectors():
+
+        """
+        mets à jour le thésaurus, un dictionnaire où:
+        clé = tuple(mot, catégorie)
+        valeur = vecteur de fréquence des contextes
+        """
+        global thesaurus
+        #liste des tokens (mot, position relative)
+        vocab = set()
+        for key in thesaurus.keys():
+                for context_key in thesaurus[key]:
+                        vocab.add(context_key)
+        #matrice diagonale
+        diag_matrix = np.eye(len(vocab))
+
+        #dictionnaire des indices de chaque token
+        word2idx = {token: index for (index, token) in enumerate(vocab)} 
+
+        #matrice de vecteurs(vides pour le moment) à renvoyer
+        context_vectors = {token: np.zeros(len(vocab)) for token in thesaurus.keys()}
+
+        #mise à jour des vecteurs
+        for token in thesaurus:
+                for context_token in thesaurus[token]:
+                        try:
+                                #màj
+                                context_vectors[token][word2idx[context_token]] = thesaurus[token][context_token] #decent for now 
+                        except KeyError:
+                                pass
+        thesaurus = context_vectors
+
+def cos_similarity(a,b):
+        """
+        renvoie la similarité entre deux vecteurs de même longueur, entre 0 et 1
+        """
+        return np.dot(a,b)/( norm(a) * norm(b))
+
+load_data()
+make_vectors()
+
+
+
+liste = list(thesaurus.keys())
+for index, key in enumerate(liste):
+        for other_key in liste[index+1:]:
+                #etant donné la taille des vecteurs, le calcul du cosinus est long
+                sim = cos_similarity(thesaurus[key], thesaurus[other_key])
+                if sim >= 0.8 and sim <1:
+                        print(key, other_key, sim)
+
+
